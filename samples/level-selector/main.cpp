fa1043567ecff68ed08e62e469127f9c3e077fc1
chrisnash <guestblogme@gmail.com>
2013-04-24 23:07:11 -0700

    Level selector demo, basic screen layout

--- samples/level-selector/main.cpp
+++ samples/level-selector/main.cpp
@@ -53,6 +53,11 @@ bool touchEvents[CUBE_ALLOCATION];
 TiltShakeRecognizer tsr[2];
 int8_t tiltEvents[2];
 
+const Sifteo::AssetImage *characters[] =
+{
+		&None, &Sully1, &Sully2, &Sully3, &Sully4
+};
+
 class SimpleHandler : public Scene::Handler
 {
 	Sifteo::AssetConfiguration<ASSET_CAPACITY> assetconf1;
@@ -114,21 +119,34 @@ public:
 			break;
 		case 1:
 			// text in a 16-pixel area
+			Font::drawCentered( v, vec(0,0), vec(128,16), el.obj<TextField>().text);
 			break;
 		case 2:
 			// text in a 32-pixel area
+			Font::drawCentered( v, vec(0,0), vec(128,32), el.obj<TextField>().text);
 			break;
 		case 3:
 			// a vertical scrolling level selector, takes some work
 			break;
 		case 4:
-			// a sprite overlay
+			// a sprite overlay, type, x, y, hidden
+			if(el.data[3])
+			{
+				v.sprites[el.data[0]].hide();
+			}
+			else
+			{
+				v.sprites[el.data[0]].setImage( el.data[0] ? Down : Up);
+				v.sprites[el.data[0]].move( el.data[1], el.data[2]);
+			}
 			break;
 		case 5:
 			// background of the character area
+			v.bg0.image(vec(0,0), Background);
 			break;
 		case 6:
 			// the character
+			v.bg1.image(vec(4,2), *characters[el.d8]);
 			break;
 		case 7:
 			// the character's scroll registers
@@ -152,7 +170,7 @@ public:
 			// a vertical scrolling level selector, takes some work
 			break;
 		case 4:
-			// a sprite overlay
+			// a sprite overlay, data is type, x, y, hidden
 			break;
 		case 5:
 			// background of the character area
@@ -198,6 +216,14 @@ public:
 	}
 };
 
+void setupSprite(Scene::Element &el, uint8_t type, uint8_t x, uint8_t y, uint8_t hidden)
+{
+	el.data[0] = type;
+	el.data[1] = x;
+	el.data[2] = y;
+	el.data[3] = hidden;
+}
+
 void main()
 {
 	// initialize scene
@@ -211,14 +237,23 @@ void main()
 	// entities
 	TextField t1_top; 		t1_top.text << "Chapter 1";
 	TextField t1_bottom;	t1_bottom.text << "Completed 0/9";
-	TextField t2_top;		t1_top.text << "Level 1";
+	TextField t2_top;		t2_top.text << "Level 1";
 	TextField t2_bottom;	t2_bottom.text << "Best time 00:59.99";
 	TextField t3;			t3.text << "Tilt and tap to select";
 
 	// mode 0 body mode 1 16 top mode 2 16 bottom mode 3 32 bottom
-
+	// put mode 0 entries first to do the asset download up front
 	// 0 scroll register
 	Scene::createElement(0);
+	// 3 level selector
+	Scene::createElement(3, 0);
+	Scene::createElement(3, 1);
+
+	// 4 sprite
+	setupSprite(Scene::createElement(4, 0), 0, 56, 0, 0);
+	setupSprite(Scene::createElement(4, 0), 1, 56, 80, 0);
+	setupSprite(Scene::createElement(4, 1), 0, 56, 0, 0);
+	setupSprite(Scene::createElement(4, 1), 1, 56, 80, 0);
 
 	// 1 16 px text fields
 	Scene::createElement(1, 0,1, &t1_top);
@@ -229,22 +264,10 @@ void main()
 	// 2 32 px text fields
 	Scene::createElement(2, 2,3, &t3);
 
-	// 3 level selector
-	Scene::createElement(3, 0);
-	Scene::createElement(3, 1);
-
-	// 4 sprite
-	Scene::createElement(4, 0);
-	Scene::createElement(4, 0);
-	Scene::createElement(4, 1);
-	Scene::createElement(4, 1);
-
 	// 5 character background
 	Scene::createElement(5, 2);
-
 	// 6 character
-	Scene::createElement(6, 2);
-
+	Scene::createElement(6, 2).d8 = 1;
 	// 7 character scroll registers
 	Scene::createElement(7, 2);
 
