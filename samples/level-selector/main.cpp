81992426b03dc1ed0b137476b5bbdc498db74a4e
chrisnash <guestblogme@gmail.com>
2013-06-24 15:38:54 -0700

    Added basic level selector functionality

--- samples/level-selector/main.cpp
+++ samples/level-selector/main.cpp
@@ -36,6 +36,95 @@ const int8_t bidiLookahead = 2;
 const int8_t scrollLookback = 1;
 const int8_t scrollLookforward = 4;
 
+const uint16_t levels[] =
+{
+		0x101, 0x102, 0x103, 0x104, 0x105, 0x106, 0x107, 0x000,
+		0x201, 0x202, 0x203, 0x204, 0x205, 0x206, 0x000, 0x000,
+		0x301, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
+		0x401, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
+		0x501, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
+		0x601, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
+		0x701, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
+};
+uint16_t chapterCount = 7;
+uint16_t chapterWidth = 8;
+
+const uint16_t startPoint = 0x0101;
+
+class SelectorLogic
+{
+	static uint16_t findIndex(uint16_t current)
+	{
+		for(uint16_t i=0; i<arraysize(levels); i++)
+		{
+			if(levels[i] == current) return i;
+		}
+		return 0;
+	}
+	static uint16_t indexOf(uint16_t cIndex, uint16_t lIndex)
+	{
+		return cIndex*chapterWidth + lIndex;
+	}
+public:
+	static uint16_t previousChapter(uint16_t current)
+	{
+		uint16_t index = findIndex(current);
+		uint16_t cIndex = index/chapterWidth; uint16_t lIndex = index%chapterWidth;
+
+		do
+		{
+			if(cIndex==0) cIndex=chapterCount;
+			cIndex--; lIndex=0;
+		}
+		while(levels[indexOf(cIndex,lIndex)]==0);
+
+		return levels[indexOf(cIndex, lIndex)];
+	}
+	static uint16_t nextChapter(uint16_t current)
+	{
+		uint16_t index = findIndex(current);
+		uint16_t cIndex = index/chapterWidth; uint16_t lIndex = index%chapterWidth;
+
+		do
+		{
+			cIndex++; lIndex=0;
+			if(cIndex==chapterCount) cIndex=0;
+		}
+		while(levels[indexOf(cIndex,lIndex)]==0);
+
+		return levels[indexOf(cIndex, lIndex)];
+	}
+
+	static uint16_t previousLevel(uint16_t current)
+	{
+		uint16_t index = findIndex(current);
+		uint16_t cIndex = index/chapterWidth; uint16_t lIndex = index%chapterWidth;
+
+		do
+		{
+			if(lIndex==0) lIndex = chapterWidth;
+			lIndex--;
+		}
+		while(levels[indexOf(cIndex,lIndex)]==0);
+		return levels[indexOf(cIndex, lIndex)];
+	}
+	static uint16_t nextLevel(uint16_t current)
+	{
+		uint16_t index = findIndex(current);
+		uint16_t cIndex = index/chapterWidth; uint16_t lIndex = index%chapterWidth;
+
+		do
+		{
+			lIndex++;
+			if(lIndex==chapterWidth) lIndex = 0;
+		}
+		while(levels[indexOf(cIndex,lIndex)]==0);
+		return levels[indexOf(cIndex, lIndex)];
+	}
+};
+
+Sifteo::BitArray<3> currentListeners;
+
 class ScrollListener
 {
 public:
@@ -75,6 +164,27 @@ public:
 		else
 		{
 			doRender(v, bufferLine, itemBuffer[imageIndex], imageOffset);
+		}
+	}
+
+	void setNextItem(uint16_t item)
+	{
+		if(itemBuffer[2] != item)
+		{
+			// if any of item 2 is in the visible area, you need to mark it as needing a redraw
+			if(windowEnd > windowSize) windowEnd = windowSize;
+			if(windowStart > windowEnd) windowStart = windowEnd = 0;
+			itemBuffer[2] = item;
+		}
+	}
+	void setPreviousItem(uint16_t item)
+	{
+		if(itemBuffer[0] != item)
+		{
+			// if any of item 0 is in the render window, you need to refresh it
+			if(windowStart < 0) windowStart = 0;
+			if(windowStart > windowEnd) windowStart = windowEnd = 0;
+			itemBuffer[0] = item;
 		}
 	}
 
@@ -143,9 +253,6 @@ public:
 		el.repaint();
 	}
 
-	virtual uint16_t nextItem(uint16_t item) = 0;
-	virtual uint16_t prevItem(uint16_t item) = 0;
-
 	void onHalfway(Scene::Element &el, int16_t direction)
 	{
 		// once halfway, change the current image and the windowing parameters
@@ -153,13 +260,13 @@ public:
 		{
 			itemBuffer[0] = itemBuffer[1];
 			itemBuffer[1] = itemBuffer[2];
-			itemBuffer[2] = nextItem(itemBuffer[2]);
+			itemBuffer[2] = 0x0000;
 		}
 		else
 		{
 			itemBuffer[2] = itemBuffer[1];
 			itemBuffer[1] = itemBuffer[0];
-			itemBuffer[0] = prevItem(itemBuffer[0]);
+			itemBuffer[0] = 0x0000;
 		}
 		// move the base address of the window down and the cached status in the opposite direction
 		windowOffset = Sifteo::umod(windowOffset + direction * windowSize, 18);
@@ -188,16 +295,6 @@ class ChapterSelector : public ScrollingSelector
 		{
 			v.bg0.fill( vec(0U, bufferLine), vec(17U, 1U), Black);
 		}
-	}
-
-	virtual uint16_t nextItem(uint16_t item)
-	{
-		return 0x0000;
-	}
-
-	virtual uint16_t prevItem(uint16_t item)
-	{
-		return 0x0000;
 	}
 
 	virtual int xoffset()
@@ -242,15 +339,21 @@ class LevelSelector : public ScrollingSelector
 	{
 		return 4;
 	}
+};
 
-	virtual uint16_t nextItem(uint16_t item)
+class CharacterAnimator : public ScrollListener
+{
+	void onBegin(Scene::Element &el, int16_t direction)
 	{
-		return 0x0000;
 	}
-
-	virtual uint16_t prevItem(uint16_t item)
+	void onUpdate(Scene::Element &el, int16_t position, int16_t direction)
 	{
-		return 0x0000;
+	}
+	void onHalfway(Scene::Element &el, int16_t direction)
+	{
+	}
+	void onComplete(Scene::Element &el, int16_t direction)
+	{
 	}
 };
 
@@ -355,7 +458,10 @@ public:
 			break;
 		case 3:
 			// a vertical scrolling level selector, takes some work
-			el.obj<ScrollingSelector>().draw(v, scrollRegister->sdata16[0] * scrollRegister->sdata16[1]);
+			if(el.cube!=2)
+			{
+				el.obj<ScrollingSelector>().draw(v, scrollRegister->sdata16[0] * scrollRegister->sdata16[1]);
+			}
 			break;
 		case 4:
 			// a sprite overlay, type, x, y, hidden
@@ -388,29 +494,29 @@ public:
 		{
 		case 0:
 			// the scroll register
+			if(el.sdata16[1])
 			{
 				int16_t next = el.sdata16[0] + fc;
 				// whether to fire the halfway event
 				if((el.sdata16[0]<scrollHalf) && (next >= scrollHalf))
 				{
-					scrollers[0].obj<ScrollListener>().onHalfway(scrollers[0], el.sdata16[1]);
-					scrollers[1].obj<ScrollListener>().onHalfway(scrollers[1], el.sdata16[1]);
+					for(uint8_t l : currentListeners)  scrollers[l].obj<ScrollListener>().onHalfway(scrollers[l], el.sdata16[1]);
 				}
 				// whether to fire the completion event (and for the moment automatically start another)
 				if((el.sdata16[0]<scrollMax) && (next >= scrollMax))
 				{
-					scrollers[0].obj<ScrollListener>().onComplete(scrollers[0], el.sdata16[1]);
-					scrollers[1].obj<ScrollListener>().onComplete(scrollers[1], el.sdata16[1]);
-
-					next -= scrollMax;
-
-					scrollers[0].obj<ScrollListener>().onBegin(scrollers[0], el.sdata16[1]);
-					scrollers[1].obj<ScrollListener>().onBegin(scrollers[1], el.sdata16[1]);
+					el.sdata16[0] = scrollMax;
+					for(uint8_t l : currentListeners) scrollers[l].obj<ScrollListener>().onUpdate(scrollers[l], el.sdata16[0], el.sdata16[1]);;
+					el.sdata16[0] = 0;
+					for(uint8_t l : currentListeners) scrollers[l].obj<ScrollListener>().onComplete(scrollers[l], el.sdata16[1]);
+					el.sdata16[1] = 0;	// and stop again
+					currentListeners.clear();
 				}
-
-				el.sdata16[0] = next;
-				scrollers[0].obj<ScrollListener>().onUpdate(scrollers[0], el.sdata16[0], el.sdata16[1]);
-				scrollers[1].obj<ScrollListener>().onUpdate(scrollers[1], el.sdata16[0], el.sdata16[1]);
+				else
+				{
+					el.sdata16[0] = next;
+					for(uint8_t l : currentListeners) scrollers[l].obj<ScrollListener>().onUpdate(scrollers[l], el.sdata16[0], el.sdata16[1]);;
+				}
 			}
 			break;
 		case 1:
@@ -421,6 +527,58 @@ public:
 			break;
 		case 3:
 			// a vertical scrolling level selector, takes some work
+			if(scrollRegister->sdata16[1]==0)	// only if not currently scrolling
+			{
+				uint16_t current = scrollers[1].obj<ScrollingSelector>().itemBuffer[1];
+				if(el.cube!=2)
+				{
+					// not scrolling, some action selected on the cube
+					if(tiltEvents[el.cube])
+					{
+						currentListeners.clear();
+						scrollRegister->sdata16[0] = 0;						// start with zero offset
+						scrollRegister->sdata16[1] = -tiltEvents[el.cube];	// scroll in the other direction
+						// start a scroll in this window
+						switch(el.cube)
+						{
+						case 0:
+							// change chapter
+							currentListeners.mark(0); currentListeners.mark(1); currentListeners.mark(2);
+							current = (tiltEvents[el.cube] < 0) ? SelectorLogic::nextChapter(current) : SelectorLogic::previousChapter(current);
+							if(tiltEvents[el.cube] < 0)
+							{
+								scrollers[0].obj<ScrollingSelector>().setNextItem(current & 0xFF00);
+								scrollers[1].obj<ScrollingSelector>().setNextItem(current);
+							}
+							else
+							{
+								scrollers[0].obj<ScrollingSelector>().setPreviousItem(current & 0xFF00);
+								scrollers[1].obj<ScrollingSelector>().setPreviousItem(current);
+							}
+							break;
+						case 1:
+							// change level
+							currentListeners.mark(1);	// just one pane listening
+							current = (tiltEvents[el.cube] < 0) ? SelectorLogic::nextLevel(current) : SelectorLogic::previousLevel(current);
+							if(tiltEvents[el.cube] < 0)
+							{
+								scrollers[1].obj<ScrollingSelector>().setNextItem(current);
+							}
+							else
+							{
+								scrollers[1].obj<ScrollingSelector>().setPreviousItem(current);
+							}
+							break;
+						}
+						// notify everyone they've started
+						for(uint8_t l : currentListeners) scrollers[l].obj<ScrollListener>().onBegin(scrollers[l], el.sdata16[1]);
+					}
+				}
+				else
+				{
+					if(touchEvents[el.cube]) return current;
+				}
+			}
 			break;
 		case 4:
 			// a sprite overlay, data is type, x, y, hidden
@@ -462,8 +620,17 @@ public:
 				{
 					tsr[i].update();
 					tiltEvents[i] = tsr[i].tilt.y;
+					// tell this scroller it needs to update
+					if(tiltEvents[i])
+					{
+						scrollers[i].setUpdate();
+					}
 				}
 				else tiltEvents[i] = 0;
+			}
+			else if(i==2)
+			{
+				if(touchEvents[i]) scrollers[i].setUpdate();	// tell the scroller on cube 3 it's time to update
 			}
 		}
 	}
@@ -479,6 +646,8 @@ void setupSprite(Scene::Element &el, uint8_t type, uint8_t x, uint8_t y, uint8_t
 
 void main()
 {
+	SCRIPT(LUA, System():setAssetLoaderBypass(true));
+
 	// initialize scene
 	Scene::initialize();
 
@@ -498,14 +667,16 @@ void main()
 	// put mode 0 entries first to do the asset download up front
 	ChapterSelector cs;
 	LevelSelector ls;
+	CharacterAnimator ca;
 
 	// 0 scroll register
 	scrollRegister = & (Scene::createElement(0).fullUpdate());
-	scrollRegister->sdata16[1] = 1;		// set scroll direction
+	scrollRegister->sdata16[1] = 0;		// set scroll direction (not scrolling)
+	currentListeners.clear();			// nobody's listening
 
 	// 3 level selector
-	void *scrollerObjects[] = {&cs, &ls};
-	scrollers = Scene::createElement(3, 0).fromTemplate(2, scrollerObjects);
+	void *scrollerObjects[] = {&cs, &ls, &ca};
+	scrollers = Scene::createElement(3, 0).fromTemplate(3, scrollerObjects);
 
 	// 4 sprite
 	setupSprite(Scene::createElement(4, 0), 0, 56, 0, 0);
@@ -528,6 +699,9 @@ void main()
 	Scene::createElement(6, 2).d8 = 1;
 	// 7 character scroll registers
 	Scene::createElement(7, 2);
+
+	cs.itemBuffer[1] = startPoint & 0xFF00;
+	ls.itemBuffer[1] = startPoint;
 
 	while(1)
 	{
